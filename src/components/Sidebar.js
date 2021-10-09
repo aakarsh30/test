a1e59f68d075142cedad406a5072e4e3113feefe
aakarsh30 <aakarshsaxenasup@gmail.com>
2021-10-09 13:50:20 +0530

    secondcommit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './sidebar.scss';
-const Icon=({image})=>{
+const Icon=({image,bgcolor,...props})=>{
     return(
-        <div className='icon'>
+        <div {...props} style={{backgroundColor:`${bgcolor}`}} className='icon'>
             <img src={image} style={{objectFit:'contain',height:'25px'}} />
         </div>
     )
 }
 function Sidebar() {
+    const [clr,setclr]=useState({
+        frst:true,
+        scnd:false,
+        thrd:false
+    });
+    
     return (
         <>
         <div className='sidebar'>
             <svg style={{height:"40px",marginLeft:"20px",marginBottom:"50px",cursor:"pointer"}} fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="48px" height="48px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
-            <Icon image='./home.png' />
-            <Icon image='./calendar.png' />
-            <Icon image='./settings.png' />
+            <Icon image='./home.png' bgcolor={clr.frst?'#0CB459':'transparent'} onClick={()=> {setclr({frst:true,scnd:false,thrd:false})}} />
+            <Icon image='./calendar.png' bgcolor={clr.scnd?'#0CB459':'transparent'} onClick={()=> {setclr({frst:false,scnd:true,thrd:false})}} />
+            <Icon image='./settings.png' bgcolor={clr.thrd?'#0CB459':'transparent'} onClick={()=> {setclr({frst:false,scnd:false,thrd:true})}} />
             <div className='exit'>
                 <img src='./logout.png' style={{height:"25px",objectFit:"contain"}} />
             </div>
